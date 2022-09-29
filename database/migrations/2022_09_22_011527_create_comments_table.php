ed5ec040963128d448ee4371ffdbfd70c4dc1ea9
samuel ojeda <2121200463@soy.utj.edu.mx>
2022-09-28 20:27:31 -0500

    Primir commit

---
+++ database/migrations/2022_09_22_011527_create_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table-> integer(column: 'user_id');
+            $table->integer(column: 'video_id');
+            $table-> string(column:'body', length: 250);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};
